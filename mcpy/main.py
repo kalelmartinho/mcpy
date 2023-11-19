195a8a56c4bfc4b339f548bf224bf085402eacef
Kalel L. Martinho <kalelmartinho@gmail.com>
2023-11-19 13:48:19 -0300

    feat: Add window and game class

--- mcpy/main.py
+++ mcpy/main.py
@@ -7,4 +7,33 @@ import pyglet.gl as gl
 
 
 class Window(pyglet.window.Window):
-    ...
+    def __init__(self, **args):
+        super(Window, self).__init__(**args)
+
+    def on_draw(self):
+        gl.glClearColor(1.0, 0.5, 1.0, 1.0)
+        self.clear()
+
+    def on_resize(self, width, height):
+        print(f"resize {width} * {height}")
+
+
+class Game:
+    def __init__(self):
+        self.config = gl.Config(double_buffer=True, minor_version=3, major_version=3)
+        self.window = Window(
+            config=self.config,
+            width=800,
+            height=600,
+            caption="Minecraft clone",
+            resizable=True,
+            vsync=False,
+        )
+
+    def run(self):
+        pyglet.app.run()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
